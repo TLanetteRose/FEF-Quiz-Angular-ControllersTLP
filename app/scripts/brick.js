4c28ed1c3da6596a114fa2721bebe8d2a483967d
T. Lanette <tlpollard4516@gmail.com>
2019-08-07 15:29:02 -0400

    Update brick.js

--- app/scripts/brick.js
+++ app/scripts/brick.js
@@ -1,1 +1,18 @@
+'use strict';
+
+/** 
+* @ngdoc overview
+* @name controllerQuizApp.controller:BrickCtrl
+* @description
+* # BrickCtrl
+* Controller of the controllerQuizApp
+*/
+
+angular.module('controllerQuizApp')
+  .controller('BrickCtrl', function() {
+  this.color = 'Green';
+  this.size = '2 x 6';
+  this.price = 0.03;
+  this.quantity = 697;
+});
 
